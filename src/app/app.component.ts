fca495d02aa494da4ae37031f7b512c9ae091167
StephanZager <Stephan-Zager@web.de>
2024-10-28 11:09:07 +0100

    folder structure created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { HeaderComponent } from './sharded/header/header.component';
+import { FooterComponent } from './sharded/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/main-content/lading-page/lading-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lading-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './lading-page.component.html',
+  styleUrl: './lading-page.component.scss'
+})
+export class LadingPageComponent {
+
+}
