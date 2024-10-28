bc662c4b7ba6c12c8f37f95ef8577b194de6830a
StephanZager <Stephan-Zager@web.de>
2024-10-28 18:05:09 +0100

    lading page html and style

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { LadingPageComponent } from './lading-page/lading-page.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [LadingPageComponent],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrls: ['./main-content.component.scss', './../../styles.scss']
 })
 export class MainContentComponent {
 
