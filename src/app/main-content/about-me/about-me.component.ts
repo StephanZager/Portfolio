e6456db360f69ff3db9591626cc2d87f0820cf65
StephanZager <Stephan-Zager@web.de>
2024-10-30 22:04:29 +0100

    about me section created

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [NgClass, CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
 
+  mindsets = [
+    {
+      imgSrc: './../../../assets/img/icons/location.png',
+      text: 'Flexible in terms of working environments, I can work effectively both on-site in Munich and remotely.'
+    },
+    {
+      imgSrc: './../../../assets/img/icons/lamp.png',
+      text: 'I am open-minded and always looking for personal challenges to constantly improve my knowledge and skills.'
+    },
+    {
+      imgSrc: './../../../assets/img/icons/puzzle.png',
+      text: 'In my profession, programming isn\'t just about writing code; it\'s a creative form of problem-solving. I take pride in my ability to distill complex technical challenges into simple, user-friendly solutions. This way, I help you achieve your goals and bring your visions to life.'
+    }
+  ];
 }
