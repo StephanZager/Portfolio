acd8f7ea3e0bae71dcfec651d472f7fc0b47eea6
StephanZager <Stephan-Zager@web.de>
2024-11-02 23:37:05 +0100

    my scill section styling and html

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LadingPageComponent } from './lading-page/lading-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LadingPageComponent, AboutMeComponent],
+  imports: [LadingPageComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss', './../../styles.scss']
 })
--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
 
+  skills = [
+    {
+      skillImg: './../../../assets/img/icons/html.png',
+      skillName: 'HTML'
+    },
+    {
+      skillImg: './../../../assets/img/icons/css.png',
+      skillName: 'CSS'
+    },
+    {
+      skillImg: './../../../assets/img/icons/javascript.png',
+      skillName: 'JavaScript'
+    },
+    {
+      skillImg: './../../../assets/img/icons/typescript.png',
+      skillName: 'TypeScript'
+    },
+    {
+      skillImg: './../../../assets/img/icons/angular.png',
+      skillName: 'Angular'
+    },
+    {
+      skillImg: './../../../assets/img/icons/firebase.png',
+      skillName: 'Firebase'
+    },
+    {
+      skillImg: './../../../assets/img/icons/git.png',
+      skillName: 'GIT'
+    },
+    {
+      skillImg: './../../../assets/img/icons/api.png',
+      skillName: 'Rest-Api'
+    },
+    {
+      skillImg: './../../../assets/img/icons/scrum.png',
+      skillName: 'Scrum'
+    },
+    {
+      skillImg: './../../../assets/img/icons/material-design.png',
+      skillName: 'Matreial Design'
+    },
+    {
+      skillImg: './../../../assets/img/icons/continually-learning.png',
+      skillName: 'Continually learning'
+    },
+  ]
 }
