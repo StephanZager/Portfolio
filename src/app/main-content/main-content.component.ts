cc54a6703eee20a99e7935067a2906f0f49de2eb
StephanZager <Stephan-Zager@web.de>
2024-11-03 18:12:24 +0100

    footer html and styling

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { LadingPageComponent } from './lading-page/lading-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from '../sharded/footer/footer.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LadingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [LadingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss', './../../styles.scss']
 })
--- src/app/sharded/footer/footer.component.ts
+++ src/app/sharded/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IconBarComponent } from '../icon-bar/icon-bar.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [IconBarComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
