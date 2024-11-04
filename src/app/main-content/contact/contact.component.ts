0cb1c0670a07002eeb0857675107ef3221be9742
StephanZager <Stephan-Zager@web.de>
2024-11-04 19:50:52 +0100

    created form, contact section and styling this

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormComponent,FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { FooterComponent } from '../sharded/footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LadingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, FooterComponent],
+  imports: [LadingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, FooterComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss', './../../styles.scss']
 })
