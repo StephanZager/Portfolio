d17c7fdb4c46f9326325b88f2fdd0382d405ddf5
StephanZager <Stephan-Zager@web.de>
2024-11-05 19:48:45 +0100

    form validation and styling error

--- src/app/main-content/contact/form/form.component.ts
+++ src/app/main-content/contact/form/form.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+
+  borderError = '1px solid red';
+  borderSuccess = '1px solid green';
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    check: false
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
 
 }
