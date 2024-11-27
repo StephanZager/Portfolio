c8e3c5482b8baa7a08f572b0ef7152f7bae397aa
StephanZager <Stephan-Zager@web.de>
2024-11-27 20:16:36 +0100

    about me section translated

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -15,15 +15,15 @@ export class AboutMeComponent {
   mindsets = [
     {
       imgSrc: './../../../assets/img/icons/location.png',
-      text: 'Flexible in terms of working environments, I can work effectively both on-site in Munich and remotely.'
+      text: 'LOCATION'
     },
     {
       imgSrc: './../../../assets/img/icons/lamp.png',
-      text: 'I am open-minded and always looking for personal challenges to constantly improve my knowledge and skills.'
+      text: 'CHALLENGE'
     },
     {
       imgSrc: './../../../assets/img/icons/puzzle.png',
-      text: 'In my profession, programming isn\'t just about writing code; it\'s a creative form of problem-solving. I take pride in my ability to distill complex technical challenges into simple, user-friendly solutions. This way, I help you achieve your goals and bring your visions to life.'
+      text: 'MISSION'
     }
   ];
 }
