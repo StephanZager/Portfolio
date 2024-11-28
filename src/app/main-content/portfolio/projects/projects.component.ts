b564c420a5ca31882a463fe58207ff0f126c6101
StephanZager <Stephan-Zager@web.de>
2024-11-28 20:15:08 +0100

    portfolio section translate

--- src/app/main-content/portfolio/projects/projects.component.ts
+++ src/app/main-content/portfolio/projects/projects.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { link } from 'fs';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
@@ -17,7 +18,7 @@ export class ProjectsComponent {
       img: './../../../assets/img/portfolio/join.png',
       name : 'Join',
       programmingLanguage: ['JavaScript','HTML','CSS','Firebase'],
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      description: 'portfolio.PROJECT.JOIN_DESCRIPTION',
       linkGithub: 'https://github.com/',
       linkLive: 'https://github.com/',
     },
@@ -25,7 +26,7 @@ export class ProjectsComponent {
       img: './../../../assets/img/portfolio/pollo-loco.png',
       name : 'El Pollo Loco',
       programmingLanguage: ['JavaScript','HTML','CSS'],
-      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pedro find coins and poison bottles to fight against the killer Chicken.',
+      description: 'portfolio.PROJECT.ELPOLLO_DESCRIPTION',
       linkGithub: 'https://github.com/StephanZager/El-Pollo-Loco',
       linkLive: '',
     }
